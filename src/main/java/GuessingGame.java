7f50327eef523e49ee84573245a7a687010177d7
tb0902 <tbruton417@gmail.com>
2024-03-09 18:15:38 -0500

    First part is done just needs work with the scanner

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class GuessingGame {
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+
+    public boolean startGame() {
+        Scanner entry = new Scanner(System.in);
+        System.out.println("In this game, you'll be asked to guess a number from 1-10. Type 'Start' to Start." );
+        String entryInput = entry.nextLine();
+
+        if (entryInput.equals("Start")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    public void runGame() {
+        System.out.println("If you input a number, I will tell you if it is too small or too large.");
+        int[] numberBank = {1,2,3,4,5,6,7,8,9,10};
+        Scanner guessedNumber = new Scanner(System.in);
+
+        while (startGame() == true) {
+            Random random = new Random();
+            int numberToGuess = random.nextInt(numberBank.length-1);
+
+            if (guessedNumber.nextInt() < numberToGuess) {
+                System.out.println("Too small.");
+            }
+            else if (guessedNumber.nextInt() < numberToGuess) {
+                System.out.println("Too large.");
+            }
+            else {
+                System.out.println("You win!");
+            }
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        GuessingGame guessingGame = new GuessingGame();
+        guessingGame.runGame();
     }
 }
